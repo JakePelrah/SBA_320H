cfa46da4f97a51f9eedb0e878bd858434b1726b1
Jacob Pelrah <pelrahjsp@gmail.com>
2024-10-04 01:59:59 -0400

    img

--- src/components/LightBox/LightBox.jsx
+++ src/components/LightBox/LightBox.jsx
@@ -29,16 +29,9 @@ export default function LightBox() {
         <div ref={lightBoxRef} className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog modal-dialog-centered">
                 <div className="modal-content">
-                    <div className="modal-header">
-                        <div className="modal-title" id="exampleModalLabel">{detailedView?.name}</div>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
                     <div className="modal-body">
                         {/* {JSON.stringify(detailedView)} */}
                         <img src={detailedView?.image_url}></img>
-                    </div>
-                    <div className="modal-footer text-center">
-                
                     </div>
                 </div>
             </div>
